8c41a7b43a594a41d2c31f2b697e2cb9aeb5af98
Asitha Rodrigo <asitharodrigo@Asithas-MacBook-Pro-2023.local>
2023-12-10 14:06:31 +0000

    First commit

---
+++ programs/solana-hello-world/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("AL4A5LDecS2GeFqpg6w1WWUcjV4qhXZzuDAF4sMjb3jW");
+
+#[program]
+pub mod solana_hello_world {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
